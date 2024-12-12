363a994a241e425282dc4c9e6b4e2e2d73f44d24
RRoyStudio <roy.rebecca98@gmail.com>
2024-12-12 14:21:49 -0600

    12-12Work

--- portfolio/src/Comp/project-home.js
+++ portfolio/src/Comp/project-home.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import "./project-home.css"; 
+import CFM from "./assets/Project-cards-01.png";
+import RRoy from "./assets/Rroy-card.png";
+import Bones from "./assets/bones-card.png";
+import Cable8 from "./assets/cable8-card.png";
+import CMB from "./assets/cmb-card.png";
+import Mocha from "./assets/mocha-card.png";
 
+// Images of each project 
+const project_list = [
+    {title: "CFM Global", card: CFM, alt: "CFm Global Project"},
+    {title: "Rroy", card: RRoy, alt: "Rroy studio Project"},
+    {title: "Bones", card: Bones, alt: "Original Bones Project"},
+    {title: "Cable8", card: Cable8, alt: "Cable8 Project"},
+    {title: "Band", card: CMB, alt: "CMB Project"},
+    {title: "Mocha", card: Mocha, alt: "Mocha Paws Project"},   
+]
 
-const project_card = ({className, title, image}) => (
-    <div className={className || ''}>
-        <img src={image} alt={title} />
-    </div>
+// Makes list items from array of images
+const Cards = () =>(
+    
+    <ul className="projects">
+        {project_list.map((project) => (
+                <li key={project.title}>
+                <img src={project.card} alt={project.alt} className={project.title} />
+                </li>
+        ))} 
+    </ul>
+    
+
 )
 
-const Projects = () =>{
-    return(
-        <div className="project-home">
-            <h1>Project Home</h1>
+//Call the cards into the project component
+const Projects = () => {
+    
+    return (
+        <div classNamre="projects">
+            <div className="project-home">
+                <div>
+                <h2 className='title'>Projects</h2> 
+                
+                </div>
+                <Cards />
+            </div>
         </div>
     )
 }
