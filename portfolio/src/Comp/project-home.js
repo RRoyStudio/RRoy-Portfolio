d4d01cfe86dca0054229f5d113a71c99a161af6a
RRoyStudio <roy.rebecca98@gmail.com>
2024-12-16 14:36:37 -0600

    12-16Work

--- portfolio/src/Comp/project-home.js
+++ portfolio/src/Comp/project-home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {motion, useTransform, useScroll} from "framer-motion"
+import {useRef} from "react";
 import "./project-home.css"; 
 import CFM from "./assets/Project-cards-01.png";
 import RRoy from "./assets/Rroy-card.png";
@@ -6,6 +8,7 @@ import Bones from "./assets/bones-card.png";
 import Cable8 from "./assets/cable8-card.png";
 import CMB from "./assets/cmb-card.png";
 import Mocha from "./assets/mocha-card.png";
+
 
 // Images of each project 
 const project_list = [
@@ -18,31 +21,51 @@ const project_list = [
 ]
 
 // Makes list items from array of images
-const Cards = () =>(
+// const Cards = () =>(
     
-    <ul className="projects">
-        {project_list.map((project) => (
-                <li key={project.title}>
-                <img src={project.card} alt={project.alt} className={project.title} />
-                </li>
-        ))} 
-    </ul>
+//     <ul className="projects">
+        
+//         {project_list.map((project) => (
+//                 <li key={project.title}>
+//                 <img src={project.card} alt={project.alt} className={project.title} />
+//                 </li>     
+//         ))}  
+//     </ul>)
     
 
-)
+
+const HorizontalScrollCarousel = () => {
+    const targetRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: targetRef
+    });
+    // where the scrolling starts and stops
+    const x = useTransform(scrollYProgress, [0, 1], ["0%", "-50%"]);
+
+    return <section ref={targetRef} className='full-carousel'>
+        <div className='scroll-container'>
+            <motion.div style={{x}} className='cards-container'>
+            {project_list.map((project) => (
+                <li key={project.title}>
+                <img src={project.card} alt={project.alt} className={project.title} />
+                </li>     
+        ))}  
+            </motion.div>
+        </div>
+    </section>
+}
+
 
 //Call the cards into the project component
 const Projects = () => {
     
     return (
-        <div classNamre="projects">
-            <div className="project-home">
-                <div>
-                <h2 className='title'>Projects</h2> 
-                
-                </div>
-                <Cards />
+        <div className="project-home">
+            <div className="project-header">
+            <h2 className='title'>Projects</h2>
             </div>
+                    <HorizontalScrollCarousel/>
+                
         </div>
     )
 }
